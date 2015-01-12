d743721c6968fa1b06eba2d7a5312ea09d6b9ce1
Apon Palanuwech <palaa159@newschool.edu>
2015-01-11 21:07:08 -0500

    setting up

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,24 @@
 /* App
+ 
+ */
 
-*/
-angular.module('app', ['ionic'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+var app = angular.module('app', ['ionic', 'ngCordova']); // defining angular world
+// Cache $http
+app.config(['$httpProvider',
+    function($httpProvider) {
+        $httpProvider.defaults.cache = true;
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+]);
 
-// .config()
+app.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+});
